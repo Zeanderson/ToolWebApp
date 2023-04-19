77581b1ce71e294b35d290d865741f5e2c4a9896
Zeanderson <zackanderson14@gmail.com>
2023-04-19 16:56:22 -0500

    Main page up, and button

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__,
 @app.route('/')
 def home():
     return render_template('home.html')
-#@app.route('/')
-#def login():
-    #return render_template('login.html')
+@app.route('/Login')
+def login():
+    return render_template('login.html')
 
  
 if __name__=='__main__':
