7c780c98ead6b1218016684dfa5a954053e1947e
Zeanderson <zackanderson14@gmail.com>
2023-04-20 08:51:05 -0500

    Button works to transfer page to page

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__, 
             template_folder='templateFiles',  # HTML 
             static_folder='staticFiles' # CSS & Images 
             )
  
-@app.route('/')
+@app.route('/',)
 def home():
     return render_template('home.html')
 @app.route('/Login')
